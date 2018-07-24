74fcc95ce6d720c58176dd6bfe1ce1e68bfecdb9
disman1229 <346025232@qq.com>
2018-07-24 22:01:36 +0800

    学习模板继承完成

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'myapp'
 urlpatterns = [
     path('',views.index),
-    path('students/',views.students)
+    path('students/',views.students),
+    path('good/<int:id>/',views.good,name='good'),
+    path('main/',views.main),
+    path('detail/',views.detail)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,16 @@ from .models import Students
 
 def index(request):
     student = Students.objects.get(pk=1)
-    return render(request,'myapp/index.html',{'stu':student,'num':10})
+    return render(request,'myapp/index.html',{'stu':student,'num':10,'str':'disman','list':['disman','disman1'],'test':'10'})
 
 def students(request):
     stulist = Students.objects.all()
     return render(request,'myapp/students.html',{'students':stulist})
+
+def good(request,id):
+    return render(request,'myapp/good.html',{'num':id})
+
+def main(request):
+    return render(request,'myapp/main.html')
+def detail(request):
+    return render(request,'myapp/detail.html')
