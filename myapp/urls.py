6e4c1ed6227c1fc1d55522a6457be3866f340eed
disman1229 <346025232@qq.com>
2018-07-25 21:39:13 +0800

    学习django模板内容完成

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('students/',views.students),
     path('good/<int:id>/',views.good,name='good'),
     path('main/',views.main),
-    path('detail/',views.detail)
+    path('detail/',views.detail),
+    path('verifycode/',views.verifycode),
+    path('verifycodefile/',views.verifycodefile),
+    path('verifycodecheck/',views.verifycodecheck)
 ]
